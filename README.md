8c247c40a0de3a40d21bf07e717a9789d1cd0724
lashad <lashad@mac.com>
2023-04-26 13:46:25 +0400

    Rewrite

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 [![Docker Hub Link][docker-img]][docker-url]
 
-Alpine-Linux-based, tiny Docker-container for Node, striving for perfection. The original version is developed by Irakli Nadareishvili, but it no longer maintenanced. You can see original version [here](https://github.com/inadarei/node-alpine)
-
-Compare [25MB](https://hub.docker.com/r/irakli/node-alpine/tags/) of this container with typical 500MB to 800MB sizes you get with Ubuntu- or CentOS-based images.
-
-### Runit Support
-
-In most production deployments we would like a process monitor to restart our node service if it ever shuts down. On Alpine Linux such lightweight process monitor is: [runit](http://smarden.org/runit/), which you can install by using one of the *-runit tagged builds.
+Alpine-Linux-based, tiny Docker-container for NodeJS. This is really small image compare around [60MB](https://hub.docker.com/r/aliengreenllc/node-alpine/tags/) of this container with typical 500MB to 800MB sizes you get with Ubuntu- or CentOS-based images.
 
 
 
@@ -17,7 +11,7 @@ In most production deployments we would like a process monitor to restart our no
 Insert at the top of your Dockerfile:
 
 ```
-FROM aliengreen/node-alpine:node18.6.0-alpine3.17-runit
+FROM aliengreenllc/node-alpine:20-alpine3.17
 ```
 
 To see all supported versions: <https://hub.docker.com/r/aliengreen/node-alpine/tags/>
@@ -29,33 +23,56 @@ To see all supported versions: <https://hub.docker.com/r/aliengreen/node-alpine/
 If you need more packages installed (e.g. make, gcc for compiling some native Node modules, or imagemagick etc.) base your new container on this one and you can use `apk` package manager that Alpine provides. For instance:
 
 ```shell
-apk search --update imagemagick
+RUN apk search --update imagemagick
 ```
 
 or:
 
 ```shell
-apk add --update make gcc g++ python linux-header
+RUN add --update make gcc g++ python linux-header
 ```
 
 
 
 ## New Release
 
-Container needs periodic maintenance to update Alpine Linux or update NodeJS. For that we need to make changes in the Dockerfile for a specific version of Alpine Linux or NodeJS. 
+Container needs periodic maintenance to update Alpine Linux or NodeJS. For that we need to make changes in the Dockerfile for a specific version of Alpine Linux or NodeJS. The script also builds Docker image by given tag. So, follow the `release.sh` script steps.
 
- - Alpine Linux update - you should make manual change in the Dockerfile. e.g. `FROM alpine:3.6` can be replaced with a newer version `FROM alpine:3.17`
- - NodeJS update - For that we can use `release.sh` script by invoking termninal command:
+ - Alpine Linux-NodeJS update - We are using `release.sh` script by invoking termninal command:
 
  ```shell
- $ ./release.sh v18.16.0
+ $ ./release.sh --tag 20-alpine3.17
  ```
 
-> Where `v18.16.0` is a NodeJS version number
+> Where `20-alpine3.17` is a NodeJS Docker official image tag name.
 
-If command execution is done without errors you can see 4 new tags: `18.16.0`, `18.16.0-runit`, `18.16` and `18.16-runit`. 
+If command execution is done without errors you can see new tag: `20-alpine3.17`, by running command `git tag`
 
-> Note: The `release.sh` script first removes old tags from GitHub and then recreates new ones. So, if the tags with above names already exist it will be removed, but it OK, don't panic.
+> Note: The `release.sh` script first removes old tag from GitHub and then recreates new one. So, if the tag with above name already exist it will be removed, but it OK, don't panic.
+
+If you need fast search for official NodeJS tag you can invoke `release.sh` script with specific argument:
+
+```shell
+$ ./release.sh --tag 20-
+```
+
+Tag names organised as follows: first number `20` is a NodeJS version number and last `3.17` is Alpine-Linux version number.
+
+### Useful commands
+
+```shell
+$ docker build -t aliengreenllc/node-alpine:20-alpine3.17 .
+$ docker run -t aliengreenllc/node-alpine:20-alpine3.17
+$ docker push aliengreenllc/node-alpine:20-alpine3.17
+
+$ docker compose up -d --force-recreate --no-deps name
+$ docker exec -it name sh
+
+$ docker compose logs -f name
+$ docker history name
+```
+
+
 
 
 
@@ -63,3 +80,5 @@ If command execution is done without errors you can see 4 new tags: `18.16.0`, `
 
 [MIT](LICENSE)
 
+[docker-img]: https://img.shields.io/badge/docker-ready-blue.svg
+[docker-url]: https://hub.docker.com/r/aliengreenllc/node-alpine
--- release.sh
+++ release.sh
@@ -1,42 +1,100 @@
-#! /usr/bin/env bash
+#!/bin/bash
+#
+#  A simple bash script to make new release from official NodeJS Docker image.
+#
+#  release.sh
+#
+#  Copyright (c) 2023 Alien Green LLC.
+#
+#  This file is part of Alien Green Projects (AGP).
+#
+#  AGP is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  AGP is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with AGP. If not, see <http://www.gnu.org/licenses/>.
+#
 
-usage() {
+#GitHub repository name (organization/reponame)
+GITHUB_ORG_NAME="aliengreen"
+GITHUB_REP_NAME="node-alpine"
+
+#Docker Hub repository name (organization/reponame)
+DOCKERHUB_ORG_NAME="aliengreenllc"
+DOCKERHUB_REP_NAME="node-alpine"
+
+# Docker CLI name
+DOCKER=docker
+
+
+# Help screen
+help() {
+  echo "Docker image release script v0.1"
+  echo "Usage: $0 <options>"
+  echo "  options:"
+  echo "     --search Show list of NodeJS official docker images by tag pattern in hub.docker.com. e.g.: 20-"
+  echo "     --tag    Tag name of official Node docker image. e.g.: 20-alpine3.17"
   echo
-  echo "usage: $0 <tag>"
-  echo "  tag - Tag name of official Node docker image. e.g.: '20-alpine3.17'"
-  echo "Example usage:"
-  echo "$0 20-alpine3.17"
+  echo "Example 1: $0 --search 20-"
+  echo "Example 2: $0 --tag 20-alpine3.17"
   exit 0;
 }
 
+docker_build() {
 
-main() {
-  while getopts ":h:" opt; do
-    case $opt in
-      h)
-        usage;
-        ;;
-    esac
-  done
-
-  shift "$((OPTIND-1))"
-
-  if [ $# -ne 1 ]; then
-    usage;
+  curl -LO "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REP_NAME}/archive/refs/tags/${tag_name}.tar.gz"
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      exit $retVal
   fi
 
+  tar xzvf "${tag_name}.tar.gz"
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      exit $retVal
+  fi
 
-  tag_name="$1"
-  # SEMVER_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]+)$"
-  # if [[ ! $node_version =~ $SEMVER_REGEX ]]; then
-  #   usage;
-  # fi
+  $DOCKER build -t "${DOCKERHUB_ORG_NAME}/${DOCKERHUB_REP_NAME}:${tag_name}" "${GITHUB_REP_NAME}-${tag_name}"
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      exit $retVal
+  fi
 
-  # if [[ $node_version =~ $SEMVER_REGEX ]]; then
-  #   node_full_number="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
-  #   node_major_number="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
-  # fi
+  # Cleanup
+  rm -rf "${GITHUB_REP_NAME}-${tag_name}"
+  rm "${tag_name}.tar.gz"
 
+  echo
+  echo "Now you can run 'docker push ${DOCKERHUB_ORG_NAME}/${DOCKERHUB_REP_NAME}:${tag_name}' to end this mess."
+  echo "To see all your tags, take a look here: https://hub.docker.com/r/${DOCKERHUB_ORG_NAME}/${DOCKERHUB_REP_NAME}/tags"
+  echo  
+}
+
+search() {
+  curl -s "https://hub.docker.com/v2/repositories/library/node/tags/?name=${ACTION_PARAM}" | grep -o '"name":"[^"]*' | grep -o '[^"]*$'
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      exit $retVal
+  fi
+}
+
+tag() {
+  echo "Release with tag name '${ACTION_PARAM}', Do you wish to continue ?"
+  select yn in "Yes" "No"; do
+      case $yn in
+      Yes) break ;;
+      No) exit ;;
+      esac
+  done
+  
+  tag_name="$ACTION_PARAM"
   curr_date=`date +%Y-%m-%d_%H%M`
   echo "Releasing: Node image '$tag_name', Refresh: $curr_date ";
 
@@ -44,32 +102,10 @@ main() {
   sed -i.bak "s/{{NODE_TAG}}/$tag_name/" Dockerfile
   sed -i.bak "s/{{DATE_TIME}}/$curr_date/" Dockerfile
 
-  # Execute twice: for just node and node with runit
-
-  # COUNTER=0
-  # while [  $COUNTER -lt 2 ]; do
-
-  # if [[ $COUNTER -eq 1 ]]; then
-
-  # Node with runit
-  # sed -i.bak "s/#!RUNIT//g" Dockerfile
-  # runit_suffix="-runit"
-  # node_full_number="$node_full_number$runit_suffix"
-  # node_major_number="$node_major_number$runit_suffix"
-
-  # fi
-
-  echo git commit -m "$tag_name" Dockerfile
-  echo git tag -d "$tag_name" # This may fail if tag doesn't yet exist. That's OK
-  echo git push origin :refs/tags/$tag_name && git tag -a $tag_name -m "$tag_name"
-  # git tag -d "$node_major_number" # This may fail if such major ver doesn't yet exist. That's OK
-  # git push origin :refs/tags/$node_major_number && git tag -a $node_major_number -m "$node_major_number"
-  # git push origin --tags
-
-    # let COUNTER=COUNTER+1
-  done
-
-  # --- END WHILE LOOP
+  git commit -m "$tag_name" Dockerfile
+  git tag -d "$tag_name" # This may fail if tag doesn't yet exist. That's OK
+  git push origin :refs/tags/$tag_name && git tag -a $tag_name -m "$tag_name"
+  git push origin --tags
 
   # Cleanup
   mv Dockerfile.untouched Dockerfile
@@ -78,6 +114,66 @@ main() {
   git commit -m "cleanup after $tag_name release " Dockerfile && git push
 
   echo "Done!"
+
+  echo "Whant to build docker image ?. The command '$DOCKER build -t "${DOCKERHUB_ORG_NAME}/${DOCKERHUB_REP_NAME}:${tag_name}" "${GITHUB_REP_NAME}-${tag_name}"' will be invoked."
+  select yn in "Yes" "No"; do
+      case $yn in
+      Yes) docker_build; exit;;
+      No) exit ;;
+      esac
+  done
+
+}
+
+main() {
+
+  POSITIONAL=()
+  while [[ $# -gt 0 ]]; do
+      key="$1"
+
+      case $key in
+      --search)
+          ACTION=search
+          ACTION_PARAM="$2"
+          shift
+          shift
+          ;;
+      --tag)
+          ACTION=tag
+          ACTION_PARAM="$2"
+          shift
+          shift
+          ;;
+      *)
+          POSITIONAL+=("$1")
+          shift
+          ;;
+      esac
+  done
+
+  set -- "${POSITIONAL[@]}" # restore positional parameters
+
+  # Here is positional parameters tail. Basically the last command line argument(s).
+  TAIL=$1
+
+  # Check dependency. If 'docker' is here.
+  # Normally docker should be here but who knows :)
+  if ! hash $DOCKER &>/dev/null; then
+      echo "Docker is not installed on Host computer !, Please make sure 'docker' CLI is accessible from shell"
+      exit 1
+  fi
+
+  if [ "$ACTION" == "tag" ]; then
+      tag;
+      exit 0
+  fi
+
+  if [ "$ACTION" == "search" ]; then
+      search;
+      exit 0
+  fi
+
+  help;
 }
 
 main "$@"
